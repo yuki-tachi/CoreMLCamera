966c094c499f7c0805ce68baa0bc5c624ae10e3c
Yuki Tachi <z.yuki.tachi@gree.net>
2020-08-08 01:50:16 +0900

    Initial Commit

---
+++ CoreMLCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMLCamera
+//
+//  Created by Yuki Tachi on 2020/08/08.
+//  Copyright © 2020 Yuki Tachi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
