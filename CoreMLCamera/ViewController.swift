eee35a7ba75aee24cc60ed2fac028aee31b2d564
Yuki Tachi <z.yuki.tachi@gree.net>
2020-09-03 00:27:52 +0900

    feat:label表示修正

--- CoreMLCamera/ViewController.swift
+++ CoreMLCamera/ViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    @IBOutlet weak var photoDisplay: UIImageView!
+    @IBOutlet weak var photoInfoDisplay: UITextView!
+    @IBAction func takePhoto(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+    }
+    
+    var imagePicker: UIImagePickerController!
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        photoDisplay.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        imagePicker.dismiss(animated: true, completion: nil)
+        imageInference(image: (info[UIImagePickerController.InfoKey.originalImage] as? UIImage)!)
     }
 
+     func imageInference(image: UIImage) {
+         guard let model = try? VNCoreMLModel(for: Resnet50().model) else {
+             fatalError("モデルをロードできません")
+         }
+     
+         let request = VNCoreMLRequest(model: model) { [weak self] request, error in
+            guard let results = request.results as? [VNClassificationObservation] else { return }
+            let displayText = results.prefix(3).compactMap { "\($0.identifier) (\(Int($0.confidence * 100))%)" }.joined(separator: "\n")
+            
+             DispatchQueue.main.async {
+                self?.photoInfoDisplay.text = displayText
+             }
+         }
+     
+         guard let ciImage = CIImage(image: image) else {
+             fatalError("画像を変換できません")
+         }
+
+         let imageHandler = VNImageRequestHandler(ciImage: ciImage)
+         
+         DispatchQueue.global(qos: .userInteractive).async {
+             do {
+                 try imageHandler.perform([request])
+             } catch {
+                 print("エラー \(error)")
+             }
+         }
+    }
 
 }
-
